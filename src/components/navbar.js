a852c5e2666158dd604b103da2d0c640c2d7158e
unknown <salasmerzouk54@gmail.com>
2024-10-17 02:03:28 +0200

    Fixed Responsiveness

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -19,7 +19,7 @@ export function Navbar() {
             </div>
             <div className="links">
                 <Link to="/"> Home</Link>
-                <Link to="/experience"> Experience </Link>
+                <Link to="/experience"> Diplômes </Link>
                 <Link to="/Projects"> Projects </Link>
             </div>
         </div>
